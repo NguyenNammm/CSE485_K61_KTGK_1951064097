7150a4aadc9ba9f73953c8c20797d6e095938e2a
NguyenNammm <94859731+NguyenNammm@users.noreply.github.com>
2022-01-09 16:58:46 +0700

    Add files via upload

--- Project/database/195104097_kttv_bdkh.sql
+++ Project/database/195104097_kttv_bdkh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th1 09, 2022 lúc 10:21 AM
+-- Thời gian đã tạo: Th1 09, 2022 lúc 10:57 AM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 8.0.12
 
@@ -28,12 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `duan` (
-  `maduan` varchar(11) NOT NULL,
-  `tenduan` varchar(11) NOT NULL,
+  `maduan` varchar(50) NOT NULL,
+  `tenduan` varchar(50) NOT NULL,
   `namthuchien` int(4) NOT NULL,
-  `linhvuc` varchar(11) NOT NULL,
-  `nhiemvu` varchar(11) NOT NULL,
-  `coquanthuchien` varchar(11) NOT NULL
+  `linhvuc` varchar(50) NOT NULL,
+  `nhiemvu` varchar(50) NOT NULL,
+  `coquanthuchien` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -41,9 +41,9 @@ CREATE TABLE `duan` (
 --
 
 INSERT INTO `duan` (`maduan`, `tenduan`, `namthuchien`, `linhvuc`, `nhiemvu`, `coquanthuchien`) VALUES
-('01', 'Đánh giá mứ', 2019, 'Biến đổi kh', 'Nghiên cứu ', 'Trung tâm Ú'),
-('02', 'Xây dựng và', 2012, 'Biến đổi kh', 'Nghiên cứu ', 'Trung tâm Ú'),
-('03', 'Cập nhật kế', 2018, 'Biến đổi kh', 'Nghiên cứu ', 'Trung tâm Ú');
+('01', 'Đánh giá mức độ khí hậu và Xây dựng kịch bản Biến ', 2019, 'Biến đổi khí hậu', 'Nghiên cứu biến đổi khí hậu', 'Trung tâm Ứng phó biến đổi khí hậu'),
+('02', 'Xây dựng và triển khai kế hoạch hành động ứng phó ', 2012, 'Biến đổi khí hậu', 'Nghiên cứu biến đổi khí hậu', 'Trung tâm Ứng phó biến đổi khí hậu'),
+('03', 'Cập nhật kế hoạch hành động ứng phó với BĐKH tỉnh ', 2018, 'Biến đổi khí hậu', 'Nghiên cứu biến đổi khí hậu', 'Trung tâm Ứng phó biến đổi khí hậu');
 
 --
 -- Chỉ mục cho các bảng đã đổ
